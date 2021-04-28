4b377442e1c79bfd439277dc3afa1fcd1bcfc58c
Alvaro <varito1@gmail.com>
2021-04-28 16:07:52 -0300

    First Commit

---
+++ pages/api/_connector.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb';
+
+let cacheDb: MongoClient;
+
+export async function connectToDatabase() {
+  if (cacheDb) {
+    return cacheDb;
+  }
+
+  const client = new MongoClient(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  cacheDb = client;
+
+  return await client.connect();
+}
